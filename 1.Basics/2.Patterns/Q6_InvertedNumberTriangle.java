26f18f8bb41abf1f76b375c0dfe3b94ccfcf1eba
kai <kaushikalex19@gmail.com>
2025-04-15 22:35:16 +0530

    pattern 6 solved

--- 1.Basics/2.Patterns/Q6_InvertedNumberTriangle.java
+++ 1.Basics/2.Patterns/Q6_InvertedNumberTriangle.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Q6_InvertedNumberTriangle {
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n;j>i;j--)
+                System.out.print(n-j+1);
+            System.out.println();
+        }
+
     }
 }
